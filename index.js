4f67dd582061f73e6a5a152106f329a854c7e842
danielgreen229 <69796780+danielgreen229@users.noreply.github.com>
2022-08-03 12:04:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
-app.listen(process.env.PORT || 3000)
+
+app.get('/send', async (req, res) => {
+  
+	const wbm = require('wbm');
+
+
+	await wbm.start().then(async () => {
+	    const contacts = [
+	        { phone: '89275699861', text: "asdzxc" },
+	     
+	    ];
+	    const message = '{{text}}';
+
+	    await wbm.send(contacts, message);
+	    await wbm.end();
+	}).then(()=> {
+		console.log("MB")
+	}).catch(err => console.log(err));
+
+
+
+
+
+  res.send('Hello Worl123d!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
